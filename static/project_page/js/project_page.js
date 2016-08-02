0a5dd44d5bda1f3937d73064f9b357db2f470689
unknown <>
2016-08-02 14:44:36 +0800

    change password&log out

--- static/project_page/js/project_page.js
+++ static/project_page/js/project_page.js
@@ -163,6 +163,52 @@ bio_pro.controller('proController', function($scope, $http, $location, $mdSidena
 		});
 	}
 	
+	//显示登出窗口
+  	$scope.showLogOutDialog = function(ev){
+  		var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+  		$mdDialog.show({
+  			controller:LogOutCtrl,
+  			templateUrl:'html/logout.tmp.html',
+  			parent:angular.element(document.body),
+  			targetEvent:ev,
+  			clickOutsideToClose:true,
+  			fullscreen:useFullScreen,
+  			locals:{$http:$http, $mdToast:$mdToast}
+  		}).then(function(answer){
+  			
+  		}, function(){
+  			
+  		});
+  		$scope.$watch(function(){
+  			return $mdMedia('xs') || $mdMedia('sm');
+  		}, function(wantsFullScreen){
+  			$scope.customFullscreen = (wantsFullScreen === true);
+  		});
+  	}
+  	
+  	//显示修改密码窗口
+  	$scope.showChangePasswordDialog = function(ev){
+  		var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+  		$mdDialog.show({
+  			controller:ChangePasswordCtrl,
+  			templateUrl:'html/change_password.tmp.html',
+  			parent:angular.element(document.body),
+  			targetEvent:ev,
+  			clickOutsideToClose:true,
+  			fullscreen:useFullScreen,
+  			locals:{$http:$http, $mdToast:$mdToast}
+  		}).then(function(answer){
+  			
+  		}, function(){
+  			
+  		});
+  		$scope.$watch(function(){
+  			return $mdMedia('xs') || $mdMedia('sm');
+  		}, function(wantsFullScreen){
+  			$scope.customFullscreen = (wantsFullScreen === true);
+  		});
+  	}
+	
 	// 页面初始化
 	$scope.init();
   	
@@ -256,6 +302,65 @@ function NewProjectCtrl($scope, $mdDialog, $http, $mdToast){
 				}
 			});
 		// }
+	}
+}
+
+function LogOutCtrl($scope, $mdDialog){
+	
+	$scope.hide = function(){
+		$mdDialog.hide();
+	}
+	
+	$scope.cancel = function(){
+		$mdDialog.cancel();
+	}
+	
+	$scope.log_out = function(){
+		$mdDialog.hide();
+		window.location.href = "../login_register/login_register.html";
+	}
+}
+
+function ChangePasswordCtrl($scope, $mdDialog, $http, $mdToast){
+	
+	$scope.old_password = "";
+	$scope.new_password = "";
+	$scope.re_password = "";
+	
+	$scope.hide = function(){
+		$mdDialog.hide();
+	}
+	
+	$scope.cancel = function(){
+		$mdDialog.cancel();
+	}
+	
+	$scope.change_password = function(old_password,new_password,re_password){
+	 	if (old_password.length == 0 || new_password.length == 0 || re_password.length == 0) {
+		 	return;
+	 	} else {
+			var login_token = JSON.parse(sessionStorage.getItem('login'));
+			var opt = {
+				url: '/home/changePassword',
+				method: 'POST',
+				data: JSON.stringify({
+					token: login_token,
+					old_password: old_password,
+					new_password: new_password,
+					re_password: re_password
+				}),
+				headers: {'Content-Type': 'application/json'}
+			};
+			$http(opt).success(function(data){
+				if (data.successful) {
+					$mdDialog.hide();
+					showToast($mdToast, "Password changed successfully");
+				} else{
+					$mdDialog.hide();
+					showToast($mdToast, "Password changed FAILED");
+				}
+			});
+	 	}
 	}
 }
 
