1094bc5abc3ec2bcfc1958566ddad953fccad4aa
unknown <>
2016-08-06 23:08:34 +0800

    system chip

--- static/system_page/js/system_page.js
+++ static/system_page/js/system_page.js
@@ -2,7 +2,8 @@ var system = angular.module('sys-app',['ngMaterial','ngAnimate']);
 
 system.controller('systemController',function($scope, $http, $location, $mdSidenav, $mdDialog, $mdMedia, $mdToast){
 	
-	$scope.compound_info = [];
+	$scope.compound_info = [];//侧边栏数据
+	$scope.compound_tags = [];//导航栏数据
 	
 	$scope.getCompoundResult = function(key_word){
 		var login_token = JSON.parse(sessionStorage.getItem('login'));
@@ -29,7 +30,7 @@ system.controller('systemController',function($scope, $http, $location, $mdSiden
 	}
 	
 	$scope.compound_by_keyboard = function($event,key_word){
-		if (event.keyCode == 13) {
+		if ($event.keyCode == 13) {
 			$scope.getCompoundResult(key_word);
 		}
 	}
@@ -55,5 +56,9 @@ system.controller('systemController',function($scope, $http, $location, $mdSiden
 				$scope.formula = data.data.formula;
 			}
 		});
+	}
+	
+	$scope.addCompoundTags = function(name){
+		$scope.compound_tags.push(name);
 	}
 });
