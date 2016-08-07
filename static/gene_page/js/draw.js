a0ab00fd8d345cf89922af10040a7ee6861d5805
ZhangYongqin <qqdhsy@126.com>
2016-08-08 03:02:26 +0800

    system click

--- static/gene_page/js/draw.js
+++ static/gene_page/js/draw.js
@@ -85,30 +85,5 @@ function draw(data){
         //更新文字坐标
         svgText.attr("x", function(d){ return d.x; })
                  .attr("y", function(d){ return d.y; });
-        (function() {
-            var pageWrap = document.getElementById( 'pagewrap' ),
-                    pages = [].slice.call( pageWrap.querySelectorAll( 'div.container' ) ),
-                    currentPage = 0,
-                    triggerLoading = [].slice.call( pageWrap.querySelectorAll( '.pageload-link' ) ),
-                    loader = new SVGLoader( document.getElementById( 'loader' ), { speedIn : 500, easingIn : mina.easeinout } );
-            function init() {
-                triggerLoading.forEach( function( trigger ) {
-                   //console.log(trigger);
-                    trigger.addEventListener( 'click', function( ev ) {
-                        ev.preventDefault();
-                        loader.show();
-                        // after some time hide loader
-                        setTimeout( function() {
-                            loader.hide();
-                            classie.removeClass( pages[ currentPage ], 'show' );
-                            // update..
-                            currentPage = currentPage ? 0 : 1;
-                            classie.addClass( pages[ currentPage ], 'show' );
-                            },1000 );
-                        } );
-                    } );
-                }
-                init();
-            })();
         });
     }
