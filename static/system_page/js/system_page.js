1717540cce2c4ca77732d4f0812a90d171924554
unknown <>
2016-10-09 20:30:36 +0800

    2.0.1 system page

--- static/system_page/js/system_page.js
+++ static/system_page/js/system_page.js
@@ -1,12 +1,13 @@
 var system = angular.module('systemApp',['ngMaterial','ngAnimate','ngTagsInput']);
 
-system.controller('systemController',function($scope, $http, $location, $mdSidenav, $mdDialog, $mdMedia, $mdToast){
+system.controller('systemController',function($scope, $http, $location, $mdToast){
 	
 	$scope.compound_info = [];//侧边栏数据
 	$scope.compound_tags = [];//导航栏数据
 	$scope.compound_tags1 = [];
 	$scope.message_show = false;//默认侧边栏信息显示
 	$scope.gene_message_show = true;//基因信息不显示
+	
 	
 	$scope.jumpToSystem = function(){
   		window.location.href = "../system_page/system_page.html";
@@ -181,6 +182,12 @@ system.controller('systemController',function($scope, $http, $location, $mdSiden
 	}
 	
 	$scope.runCompoundTags = function(){
+		
+		$('#my-svg').shCircleLoader({
+			namespace: 'runLoad',
+		});
+    	$(".runLoad").css("top", $(window).height() * 0.35);
+    	
 		var login_token = JSON.parse(sessionStorage.getItem('login'));
 		var opt = {
 			url: '/system/getRelatedCompound',
@@ -193,133 +200,13 @@ system.controller('systemController',function($scope, $http, $location, $mdSiden
 		};
 		$http(opt).success(function(data){
 			if(data.successful){
+				
 				draw(data.data);
 			}
 		});
 		
 	}
-	
-	//显示修改密码窗口
-//	$scope.showChangePasswordDialog = function(ev){
-//		var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
-//		$mdDialog.show({
-//			controller:ChangePasswordCtrl,
-//			templateUrl:'html/change_password.tmp.html',
-//			parent:angular.element(document.body),
-//			targetEvent:ev,
-//			clickOutsideToClose:true,
-//			fullscreen:useFullScreen,
-//			locals:{$http:$http, $mdToast:$mdToast}
-//		}).then(function(answer){
-//			
-//		}, function(){
-//			
-//		});
-//		$scope.$watch(function(){
-//			return $mdMedia('xs') || $mdMedia('sm');
-//		}, function(wantsFullScreen){
-//			$scope.customFullscreen = (wantsFullScreen === true);
-//		});
-//	}
-  	
-  	//显示登出窗口
-//	$scope.showLogOutDialog = function(ev){
-//		var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
-//		$mdDialog.show({
-//			controller:LogOutCtrl,
-//			templateUrl:'html/logout.tmp.html',
-//			parent:angular.element(document.body),
-//			targetEvent:ev,
-//			clickOutsideToClose:true,
-//			fullscreen:useFullScreen,
-//			locals:{$http:$http, $mdToast:$mdToast}
-//		}).then(function(answer){
-//			
-//		}, function(){
-//			
-//		});
-//		$scope.$watch(function(){
-//			return $mdMedia('xs') || $mdMedia('sm');
-//		}, function(wantsFullScreen){
-//			$scope.customFullscreen = (wantsFullScreen === true);
-//		});
-//	}
 });
-
-//function ChangePasswordCtrl($scope, $mdDialog, $http, $mdToast){
-//	
-//	$scope.old_password = "";
-//	$scope.new_password = "";
-//	$scope.re_password = "";
-//	
-//	$scope.hide = function(){
-//		$mdDialog.hide();
-//	}
-//	
-//	$scope.cancel = function(){
-//		$mdDialog.cancel();
-//	}
-//	
-//	$scope.change_password = function(old_password,new_password,re_password){
-//	 	if (old_password.length == 0 || new_password.length == 0 || re_password.length == 0) {
-//		 	return;
-//	 	} else {
-//			var login_token = JSON.parse(sessionStorage.getItem('login'));
-//			var opt = {
-//				url: '/accounts/changePassword',
-//				method: 'POST',
-//				data: JSON.stringify({
-//					token: login_token,
-//					old_password: old_password,
-//					new_password: new_password,
-//					re_password: re_password
-//				}),
-//				headers: {'Content-Type': 'application/json'}
-//			};
-//			$http(opt).success(function(data){
-//				if (data.successful) {
-//					$mdDialog.hide();
-//					showToast($mdToast, "Password changed successfully");
-//				} else{
-//					$mdDialog.hide();
-//					showToast($mdToast, "Password changed FAILED");
-//				}
-//			});
-//	 	}
-//	}
-//}
-
-//function LogOutCtrl($scope, $mdDialog, $http){
-//	
-//	$scope.hide = function(){
-//		$mdDialog.hide();
-//	}
-//	
-//	$scope.cancel = function(){
-//		$mdDialog.cancel();
-//	}
-//	
-//	$scope.log_out = function(){
-//		var login_token = JSON.parse(sessionStorage.getItem('login'));
-//		var opt = {
-//			url: '/accounts/logout',
-//			method: 'POST',
-//			data: JSON.stringify({
-//				token: login_token,
-//			}),
-//			headers: {'Content-Type': 'application/json'}
-//		};
-//		$http(opt).success(function(data){
-//			if (data.successful) {
-//				$mdDialog.hide();
-//				window.location.href = "../login_register/login_register.html";
-//			} else{
-//				
-//			}
-//		});
-//		
-//	}
-//}
 
 var last = {
 	bottom: true,
