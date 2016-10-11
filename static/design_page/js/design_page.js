6340ddb9feda47fa77f6b475e1c3729d07c5f94b
unknown <>
2016-10-11 21:53:35 +0800

    2.0.3 design page

--- static/design_page/js/design_page.js
+++ static/design_page/js/design_page.js
@@ -39,13 +39,9 @@ editPro.controller('designController', function($scope, $http, $location, $mdToa
     	var login_token = JSON.parse(sessionStorage.getItem('login'));
 		var chain_id = JSON.parse(sessionStorage.getItem('chain_id'));
 		var project_id = JSON.parse(sessionStorage.getItem('project_id'));
-		// console.log(newVal);
 
 		var part_id = scope.chain_info[scope.chain_info.length-1].part_id;
 		var part_id_before = scope.chain_info[scope.chain_info.length-2].part_id;
-		//console.log(scope.chain_info);
-		//var part_id_before = $scope.chain_info[newVal.length - 2].part_id;
-		// console.log($scope.chain_info[(newVal.length - 2)]);
 
         $scope.getMrkvChain(part_id_before, part_id);
     	$scope.computeBackground();
@@ -64,7 +60,6 @@ editPro.controller('designController', function($scope, $http, $location, $mdToa
 
 		$http(opt).success(function(data) {
 			if (data.successful) {
-//				showToast($mdToast,"save SUCCESS!");
 				$scope.delete_gene = [];
 			}
 		});
